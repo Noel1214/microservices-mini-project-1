9d229db209e5f0cd8862b0c50e219a4299e0290c
Noel Sebu <noelsebu555@gmail.com>
2025-01-31 21:55:17 +0530

    simple checking everything works

--- posts/index.js
+++ posts/index.js
@@ -1,13 +1,27 @@
-const express = require(express);
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
 
 app.get("/post", (req, res) => {
-    
-})
+  res.send(posts);
+});
 
 app.post("/post", (req, res) => {
+  const { title } = req.body;
+  const id = randomBytes(4).toString("hex");
 
-})
+  posts[id] = {
+    id,
+    title,
+  };
+  res.status(201).send(posts[id]);
+});
 
-app.listen
+app.listen(4000, () => {
+  console.log("listening on port 4000");
+});
